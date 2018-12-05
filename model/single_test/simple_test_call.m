d47d568095042ab2a09d094877552adb57bcf63a
unknown <zhouxinyu882003@163.com>
2018-12-05 15:35:07 +0800

    simple_test_call.m revised: figure saved

--- model/single_test/simple_test_call.m
+++ model/single_test/simple_test_call.m
@@ -1,19 +1,17 @@
-%function simple_test_call(N_grp,lag)
-    
 N_grp=5;
 lag=0;
 
 
-    %%
-    a.project_path       = 'D:\Projects\Eqt'; 
-    cd(a.project_path); addpath(genpath(a.project_path));
-    %%
-    [a,p,stk_status_table,is_suspended_table,...
-                  rtn_table,freecap_table,sectors_table,rebalance_dates] = set_single_test();
+%%
+a.project_path       = 'D:\Projects\Eqt'; 
+cd(a.project_path); addpath(genpath(a.project_path));
+%%
+[a,p,stk_status_table,is_suspended_table,...
+              rtn_table,freecap_table,sectors_table,rebalance_dates] = set_single_test();
 
-    c = get_file_names(a.single_test.descriptors);
+c = get_file_names(a.single_test.descriptors);
     
-for i=1:1
+for i=1:length(c)
     tgt_file = cell2mat(c(i));
     tgt_tag = get_tag([a.single_test.descriptors,'\',tgt_file]);
 
@@ -23,7 +21,7 @@ for i=1:1
     style_table = del_suspended(style_table,stk_status_table,is_suspended_table);
 
     %[nav_grp,weight_grp,nav_bench] = simple_test(N_grp,rebalance_dates,rtn_table,style_table,freecap_table);
-    [nav_grp,weight_grp,nav_bench] = sector_neutral_test(N_grp,rebalance_dates,rtn_table,style_table,sectors_table,freecap_table); %#ok<ASGLU>
+    [nav_grp,weight_grp,nav_bench] = sector_neutral_test(N_grp,rebalance_dates,rtn_table,style_table,sectors_table,freecap_table); 
 
     % lag 10 day ic
     [ic, ic_ir, fr] = style_stats(rebalance_dates, style_table, rtn_table, lag);
@@ -31,5 +29,5 @@ for i=1:1
     grp_stats(rebalance_dates,nav_grp,nav_bench,lag);
 
     save(['D:\Projects\scratch_data\single_test\sector_neutral_',file2name(tgt_file),'.mat'],'nav_grp','weight_grp','ic','ic_ir','fr');
+    saveas(gcf,['D:\Projects\scratch_data\figures\',file2name(tgt_file),'.jpg']);
 end
-%end
