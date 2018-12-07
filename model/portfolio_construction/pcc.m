65bf75e35e5f625c1a5415ac9bcdeffaa46fb2a0
unknown <zhouxinyu882003@163.com>
2018-12-08 02:42:54 +0800

    portfolio_construction added

---
+++ model/portfolio_construction/pcc.m
@@ -0,0 +1,69 @@
+N_grp=5;
+lag=0;
+
+%%
+a.project_path       = 'D:\Projects\Eqt'; 
+cd(a.project_path); addpath(genpath(a.project_path));
+%%
+
+start_dt = datenum(2010,01,01);
+[a,p,stk_status_table,is_suspended_table,...
+              rtn_table,freecap_table,sectors_table,rebalance_dates] = set_single_test(start_dt);
+
+c = get_file_names(a.single_test.descriptors);
+
+% risk adjusted factor等等需要做中性的因子名
+Ind_names=cell(1,36); % 行业因子名 Ind1, Ind2...
+for i=1:36
+    Ind_names(i) = {strcat('Ind',num2str(i))};
+end
+risk_factor_names = {'beta','tcap'}; % 风格因子名
+risk_factor_names = [risk_factor_names,Ind_names];
+
+
+for i = 1:1%length(c)
+    
+    tgt_file = cell2mat(c(i));
+    tgt_tag = get_tag([a.single_test.descriptors,'\',tgt_file]);
+
+    % 读取对应的因子数据
+    style_table = h5_table(a.single_test.descriptors,tgt_file,tgt_tag);
+    % 将异常交易日改为NaN
+    style_table = del_suspended(style_table,stk_status_table,is_suspended_table);
+    
+    weight_table = portfolio_construction(a,rebalance_dates,style_table,freecap_table,risk_factor_names);
+    
+%     adj_style_table = risk_adj_factor(a,rebalance_dates,style_table,freecap_table,risk_factor_names);
+% 
+%     [nav_grp,weight_grp,nav_bench] = simple_test(5,rebalance_dates,rtn_table,adj_style_table,freecap_table);
+%     % lag 10 day ic
+%     [ic, ic_ir, fr] = style_stats(rebalance_dates, style_table, rtn_table, lag);
+% 
+%     [ls_rtn,ls_nav,mean_ret,hit_ratio,ls_ir,max_dd] = grp_stats(rebalance_dates,nav_grp,nav_bench,lag);
+% 
+%     save(['D:\Projects\scratch_data\risk_adj_test\',file2name(tgt_file),'.mat'],'nav_grp','weight_grp',...
+%                                                                     'nav_bench','ic','ic_ir','fr','ls_rtn','ls_nav',...
+%                                                                     'mean_ret','hit_ratio','ls_ir','max_dd');
+%     saveas(gcf,['D:\Projects\scratch_data\risk_adj_figures\',file2name(tgt_file),'.jpg']);
+    
+end
+
+
+% 设置CVX和Mosek
+%cvx_solver Mosek;
+%javaaddpath 'D:\Program Files\Mosek\8\tools\platform\win64x86\bin\mosekmatlab.jar'
+
+% pure factor call
+%weight_table = pure_factor(a,rebalance_dates,style_table,freecap_table,risk_factor_names);
+%save('D:\Projects\scratch_data\single_test\pure_factor.mat','weight_table');
+
+% factor mimicking portfolio call
+%weight_table = factor_mimicking(a,rebalance_dates,style_table,freecap_table,risk_factor_names);
+%save('D:\Projects\scratch_data\single_test\factor_mimicking.mat','weight_table');
+
+% risk adjusted factor call
+% adj_style_table = risk_adj_factor(a,rebalance_dates,style_table,freecap_table,risk_factor_names);
+
+% simple single factor test call
+% [nav_grp,weight_grp,nav_bench] = simple_test(5,rebalance_dates,rtn_table,adj_style_table,freecap_table);
+% save('D:\Projects\scratch_data\single_test\risk_adj_test.mat','nav_grp','weight_grp','adj_style_table');
---
+++ model/portfolio_construction/portfolio_construction.m
@@ -0,0 +1,167 @@
+% 计算纯因子组合权重, 即对style的暴露是1, risk factor和行业暴露都为0的最小风险组合
+%%
+% 若只想在一个指数成分范围内做分析, 则只需要把style_table中的其他票都设为NaN即可
+%% 问题
+% 还有个问题, 这里style是单独做的正态化, risk factors却是做risk之前在全市场范围做的正态化
+%%
+function weight_table = portfolio_construction(a,rebalance_dates,style_table,markcap_table,risk_factor_names)
+
+    dt = table2array(style_table(:,1));
+
+    % 初始化weight
+    weight = nan(length(rebalance_dates),width(style_table)-1);    
+    weight_table = [array2table(rebalance_dates),array2table(weight)];
+    weight_table.Properties.VariableNames = style_table.Properties.VariableNames;
+    
+    % 按日循环
+    for i=1:length(rebalance_dates)
+        
+       % 日期字符串
+       date = datestr(rebalance_dates(i),'yyyy-mm-dd'); 
+       % a.regression是读取回归所用矩阵的文件夹地址
+       % 暂时看是D:\Capricorn\model\risk\regression\
+       filename = [a.single_test.regression,'\Index0_',date,'.mat'];
+       
+       % 判断文件是否存在
+       if(exist(filename,'file')==2)
+           load(filename);
+       else
+           continue;
+       end
+       
+       % try catch避免当日对应的风险因子不存在
+       try
+           risk_factors = table2array(pre_reg(:,risk_factor_names));  %#ok<NODEF>
+           factor_rtn = table2array(factor_rtn(1,risk_factor_names));
+       catch
+           continue;
+       end
+       
+       % 东方金工风险因子数据文件名, 是用py扒下来的
+       % cov存在D;\Capricorn\model\dfquant_risk\cov\中
+       % risk factors存在D:\Capricorn\model\dfquant_risk\factors\中
+       % spec存在D:\Capricorn\model\dfquant_risk\spec中
+       % 日期字符串换成yyyymmdd格式
+       date = datestr(rebalance_dates(i),'yyyymmdd'); 
+       cov_filename = [a.single_test.dfquant_risk,'\cov\cov_',date,'.csv'];
+       factor_filename = [a.single_test.dfquant_risk,'\factors\risk_factors_',date,'.csv'];
+       spec_filename = [a.single_test.dfquant_risk,'\spec\spec_',date,'.csv'];
+       
+       if(exist(cov_filename,'file')==2 && exist(factor_filename,'file')==2 && exist(spec_filename,'file')==2)
+           cov = readtable(cov_filename);
+           spec = readtable(spec_filename);
+           factors = readtable(factor_filename);
+           
+           % 把东方金工数据中的行名称从数字转到SH600018这种格式
+           stk_num = factors(2:end,1);
+           stk_num = table2array(stk_num);
+           stk_codes = df_stk_codes(stk_num);
+           
+           % 东方金工数据
+           cov = table2array(cov(2:end,2:end));
+           spec = table2array(spec(:,2));
+           factors = table2array(factors(2:end,2:end));
+           
+           % 定义stk_cov即股票间cov矩阵, 行名列名都用SH600018格式股票代码
+           stk_cov = nan(width(style_table));
+           stk_cov = array2table(stk_cov,'VariableNames',style_table.Properties.VariableNames,'RowNames',style_table.Properties.VariableNames);
+           
+           % 从东方金工数据中计算股票间cov
+           df_stk_cov = factors * cov * factors' + diag(spec);
+           
+           % 用格式化后的股票代码做indexing
+           stk_cov(stk_codes,stk_codes) = array2table(df_stk_cov);
+           
+           tbl_factors = array2table(factors,'RowNames',stk_codes);
+           tbl_spec = array2table(spec,'RowNames',stk_codes);
+           
+           
+       else
+           continue;
+       end
+       
+       
+       if(all(any(isnan(risk_factors),2)))
+           continue;
+       end       
+       
+       % 当日回归矩阵中的股票代码
+       stk_codes = pre_reg.Properties.RowNames;
+       
+       % 用股票代码筛选style中需要的数据
+       % 截取style中同样代码股票
+       j = find(ismember(dt,rebalance_dates(i)),1,'first');
+       style = style_table(j,stk_codes);
+       style = table2array(style)';
+       cap = markcap_table(j,stk_codes);
+       cap = table2array(cap)';
+       
+       % 对应的股票协方差矩阵
+       stk_cov = stk_cov(stk_codes,stk_codes);
+       stk_cov = table2array(stk_cov);
+       
+       %......
+       factors = tbl_factors(stk_codes,:);
+       factors = table2array(factors);
+       spec = tbl_spec(stk_codes,1);
+       spec = table2array(spec);
+       
+       % 优化求解
+       %weight_table(i,stk_codes) = array2table(minvol_opt(style,cap,risk_factors,stk_cov)');
+       exp_bound = ones(size(cov,1),1) * 0.1;
+       active_bound = ones(size(factors,1),1) * 0.02;
+       weight_table(i,stk_codes) = array2table(optimizer(0.1,risk_factors,factor_rtn',cov,factors,spec,exp_bound,active_bound)');%%%%%%
+        
+       disp(date);
+    end
+        
+end
+
+
+function x = optimizer(lambda, alpha_factors, alpha_factors_rtn, factor_cov, exposure, spk, exp_bound, active_bound)
+%OPTIMIZER 此处显示有关此函数的摘要
+%   此处显示详细说明
+
+    bound_idx = exp_bound<Inf;
+    bound_mtx = exposure(:,bound_idx);
+    bound = exp_bound(bound_idx);
+    
+    alpha_factors(isnan(alpha_factors))=0;
+    alpha_factors_rtn(isnan(alpha_factors_rtn))=0;
+
+    % 这里还要考虑去NaN        
+    n = size(exposure,1); %#ok<NASGU>
+    cvx_begin
+        variable x(n)
+        maximize(alpha_factors_rtn' * alpha_factors' * x - lambda * quad_form(exposure' * x,factor_cov) - lambda * sum(spk .* x))
+        subject to
+        	%x >= 0; %#ok<VUNUS>
+            sum(x) == 0; %#ok<EQEFF>
+            -bound <= bound_mtx' * x <= bound; %#ok<VUNUS>
+            -active_bound <= x <= active_bound; %#ok<VUNUS>
+    cvx_end
+
+
+end
+
+% 从东方金工的模型结果中读取的股票代码转为SH600018这种格式
+function stk_codes = df_stk_codes(stk_num)
+
+    stk_codes = cell(length(stk_num),1);
+    for i=1:length(stk_num)
+        stk_str = num2str(stk_num(i));
+        if(length(stk_str)<6)
+            stk_str = [repmat('0',1,8-length(stk_str)),stk_str]; %#ok<AGROW>
+            stk_str(1:2) = 'SZ';
+        else
+            if(stk_str(1)=='6' || stk_str(1)=='T') % 还有个T00018是上港集箱后来退市, 不过东方的数据应该没影响
+                stk_str = ['SH',stk_str]; %#ok<AGROW>
+            else
+                stk_str = ['SZ',stk_str]; %#ok<AGROW>
+            end
+        end
+        stk_codes(i) = {stk_str};
+    end
+
+end
+
