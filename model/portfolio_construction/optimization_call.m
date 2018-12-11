24078670a8b00c5093897531a75d45bb7b2646bf
unknown <zhouxy@cmfdev.cmfchina.com>
2018-12-11 16:40:21 +0800

    optimizer: spk.*x corrected to spk.*x.*x

--- model/portfolio_construction/optimization_call.m
+++ model/portfolio_construction/optimization_call.m
@@ -15,16 +15,16 @@ p.optimization.trading_dates = p.all_trading_dates;
 c = get_file_names(a.optimization.descriptors);
 
 % risk adjusted factor等等需要做中性的因子名
-Ind_names=cell(1,36); % 行业因子名 Ind1, Ind2...
-for i=1:36
-    Ind_names(i) = {strcat('Ind',num2str(i))};
-end
-risk_factor_names = {'beta','tcap'}; % 风格因子名
-risk_factor_names = [risk_factor_names,Ind_names];
+% Ind_names=cell(1,36); % 行业因子名 Ind1, Ind2...
+% for i=1:36
+%     Ind_names(i) = {strcat('Ind',num2str(i))};
+% end
+alpha_factor_names = {'bp','earning_quality','earning_variability','growth','leverage','momentum','profitability','uncertain','volatility','yield'}; % 风格因子名
+%alpha_factor_names = [risk_factor_names];
 
 
 for i = 1:1%length(c)
     
-    weight_table = optimization(a,p,rebalance_dates,risk_factor_names);
+    weight_table = optimization(a,p,rebalance_dates,alpha_factor_names);
     
 end
