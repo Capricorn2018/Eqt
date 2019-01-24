972e7e62b5123b9b7f7649dd6fb480b51399c513
unknown <zhouxy@cmfdev.cmfchina.com>
2019-01-24 17:07:06 +0800

    alpha_factors.m: add factors

--- alpha_factor.m
+++ alpha_factor.m
@@ -10,8 +10,36 @@ p.all_trading_dates  = datenum_h5 (h5read([a.input_data_path,'\fdata\base_data\s
 p.stk_codes_         = h5read([a.input_data_path,'\fdata\base_data\securites_dates.h5'],'/stk_code'); 
 p.stk_codes          = stk_code_h5(h5read([a.input_data_path,'\fdata\base_data\securites_dates.h5'],'/stk_code')); 
 
+%% Value
+%ebpntm(p,a) % next 12 month 预期bp
+% lack of: EP_LYR 
+epttm(p,a); % EP_TTM
+eepntm(p,a); % EP_Fwd12M
+
+cfpttm(p,a); % CashFlowYield_TTM
+
+bp(p,a); % BP_LR
+
 %%
-bp(p,a)
-ebpntm(p,a) % next 12 month 预期bp
+
+%% Growth
+
+
+
+%%
+
+%% Quality
+
+
+%%
+
+%% Momentum
+%%
+
+%% Sentiment
+%%
+
+%% Technical
+%%
 cal_stk_ivol(p,a,63,16,1,{'windA'}) % 历史波动率，D=63天，半衰期16天，在wind全A中加权
 cal_stk_rtn(p,a,21,252,1) % 1Y前~1m前的收益
