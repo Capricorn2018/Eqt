cb0979e44d0f10ba6e72969cb8cae262b97e674c
unknown <zhouxy@cmfdev.cmfchina.com>
2019-02-13 12:53:51 +0800

    minor change

--- pit_data/do_pit.m
+++ pit_data/do_pit.m
@@ -4,18 +4,18 @@ end_dt = '20190201';
 n_rpt = 21;
 
 % balancesheet
- out_path = 'D:/Projects/pit_data/mat/balancesheet/';
- data_file = 'D:/Projects/pit_data/origin_data/asharebalancesheet.txt';
- sample_file = 'D:/Projects/pit_data/origin_data/sample_asharebalancesheet.csv';
- data = preprocessing(data_file,sample_file);
- pit_data(data, start_dt, end_dt, n_rpt, out_path, false);
+out_path = 'D:/Projects/pit_data/mat/balancesheet/';
+data_file = 'D:/Projects/pit_data/origin_data/asharebalancesheet.txt';
+sample_file = 'D:/Projects/pit_data/origin_data/sample_asharebalancesheet.csv';
+data = preprocessing(data_file,sample_file);
+pit_data(data, start_dt, end_dt, n_rpt, out_path, false);
 
 % income
-% out_path = 'D:/Projects/pit_data/mat/income/';
-% data_file = 'D:/Projects/pit_data/origin_data/ashareincome.txt';
-% sample_file = 'D:/Projects/pit_data/origin_data/sample_ashareincome.csv';
-% data = preprocessing(data_file,sample_file);
-% pit_data(data, start_dt, end_dt, n_rpt,out_path, true)
+out_path = 'D:/Projects/pit_data/mat/income/';
+data_file = 'D:/Projects/pit_data/origin_data/ashareincome.txt';
+sample_file = 'D:/Projects/pit_data/origin_data/sample_ashareincome.csv';
+data = preprocessing(data_file,sample_file);
+pit_data(data, start_dt, end_dt, n_rpt,out_path, true);
 
 % cashflow
 out_path = 'D:/Projects/pit_data/mat/cashflow/';
