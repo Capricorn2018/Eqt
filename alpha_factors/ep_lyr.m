0a65bb81939548ae162c32badcecd9c7d5988411
unknown <zhouxinyu882003@163.com>
2019-03-05 16:11:26 +0800

    alpha_factors.m changed

--- alpha_factors/ep_lyr.m
+++
@@ -1,6 +0,0 @@
-function [ output_args ] = ep_lyr()
-% ep_lyr: 净利润(不含少数股东损益)_最新年报 / 总市值
-    e_lyr = 0;
-    
-end
-
---
+++ alpha_factors/reports_data.m
@@ -0,0 +1,28 @@
+% 从pit_data中提取TTM和最新报表数据
+
+% input_folder = 'D:/Projects/pit_data/mat/income/';
+% db_names = {'net_profit_excl_min_int_inc','net_profit_incl_min_int_inc','oper_profit',...
+%                 'oper_rev','less_oper_cost','less_selling_dist_exp','less_gerl_admin_exp','less_fin_exp'};
+% output_folder = 'D:/Projects/pit_data/mat/alpha_factors/';
+% ttm(input_folder, stk_codes, db_names, output_folder);
+% 
+% input_folder = 'D:/Projects/pit_data/mat/cashflow/';
+% db_names = {'net_cashflows_oper_act'};
+% output_folder = 'D:/Projects/pit_data/mat/alpha_factors/';
+% ttm(input_folder, stk_codes, db_names, output_folder);
+% 
+% input_folder = 'D:/Projects/pit_data/mat/balancesheet/';
+% db_names = {'tot_shrhldr_eqt_excl_min_int','tot_non_cur_liab','monetary_cap','tot_assets'};
+% output_folder = 'D:/Projects/pit_data/mat/alpha_factors/';
+% rpt_type = 'LR';
+% latest_rpt(input_folder, stk_codes, db_names, output_folder, rpt_type);
+% 
+% 
+% input_folder = 'D:/Projects/pit_data/mat/income/';
+% db_names = {'net_profit_excl_min_int_inc','net_profit_incl_min_int_inc','oper_profit',...
+%                 'oper_rev','less_oper_cost'};
+% output_folder = 'D:/Projects/pit_data/mat/alpha_factors/';
+% rpt_type = 'SQ';
+% latest_rpt(input_folder, stk_codes, db_names, output_folder, rpt_type);
+
+
