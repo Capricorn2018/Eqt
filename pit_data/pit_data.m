7ae42fc4427518a96f25e497a57de6affebaf452
unknown <zhouxy@cmfdev.cmfchina.com>
2019-03-05 09:38:11 +0800

    pit_data.m comments corrected

--- pit_data/pit_data.m
+++ pit_data/pit_data.m
@@ -35,13 +35,16 @@ function [] = pit_data(data, start_dt, end_dt, n_rpt, ...
     data.rank_ann = data.rank_rpt;
     
     % 选择公布日在第一个交易日之前的数据
-    data_last = data(data.actual_ann_dt <= t & data.ann_dt <= t,:);
+    data_last = data(data.actual_ann_dt <= t,:);
     
+    %% data_last = gen_latest_pit(data_last,update);
     % 用get_ranks给报告期和公布日排序
     data_last = get_ranks(data_last);
     
     % 筛选前n_rpt个报告期的最新公布日
     data_last = data_last(data_last.rank_rpt<=n_rpt & data_last.rank_ann==1,:);
+    
+    %%
     
     if(do_single)
         single = single_season(data_last); %#ok<NASGU>
@@ -60,6 +63,7 @@ function [] = pit_data(data, start_dt, end_dt, n_rpt, ...
         
         update = data(data.actual_ann_dt <= t & data.actual_ann_dt > lt,:);
         
+        %% data_last = gen_latest_pit(data_last,update);
         if(size(update,1)>0)
         
             tmp_data = [data_last;update]; % 把更新的数据加在上一交易日数据 后
@@ -70,6 +74,7 @@ function [] = pit_data(data, start_dt, end_dt, n_rpt, ...
             data_last = data_last(data_last.rank_rpt<=n_rpt & data_last.rank_ann==1,:);
         
         end
+        %%
         
         % 若需要拆单季数据
         if(do_single)
@@ -83,5 +88,29 @@ function [] = pit_data(data, start_dt, end_dt, n_rpt, ...
         
     end
     
+end
+
+
+function pit = gen_latest_pit(data_last,update)
+
+    if(size(update,1)>0)        
+        
+        if(size(data_last,1)==0)
+            tmp_data = update;
+        else
+            tmp_data = [data_last;update]; % 把更新的数据加在上一交易日数据后
+        end
+
+        data_last = get_ranks(tmp_data); % 对每个股票的报告期排序、对每个报告期的ann_dt排序
+
+        % 筛选前n_rpt个报告期的最新数据
+        pit = data_last(data_last.rank_rpt<=n_rpt & data_last.rank_ann==1,:);
+        
+    else
+        pit = data_last;
+    end
+    
+    %% [~,ia,~] = unique(pit(:,{'s_info_windcode','report_period','actual_ann_dt'}));
+    %% pit = pit(ia,:);
 end
 
