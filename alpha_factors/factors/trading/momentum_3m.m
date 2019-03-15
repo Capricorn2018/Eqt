c4b81465144bca851c80de81e225da41b9e4472c
unknown <zhouxy@cmfdev.cmfchina.com>
2019-03-15 17:25:29 +0800

    any?

--- alpha_factors/factors/trading/momentum_3m.m
+++ alpha_factors/factors/trading/momentum_3m.m
@@ -2,8 +2,7 @@ function [] = momentum_3m(p,a)
 % 近3个月return
 
     D1 = 0;
-    D2 = 60; % 假设21交易日
-    if_mix = false; % 不用行业平均做shrinkage
+    D2 = 60;
 
     % 后面基本都是cal_stk_rtn.m的代码
     
@@ -14,56 +13,45 @@ function [] = momentum_3m(p,a)
 
     [S,momentum_3m] =  check_exist(tgt_file,['/',tgt_tag],p,T,N);
 
-    if S>0      
-       if if_mix
-          vi = sector_rtn(a,D1,D2,S,T,momentum_3m); %#ok<UNRCH>
-       end
+    if S>0
 
-       adj_prices   = h5read([a.input_data_path,'\fdata\base_data\stk_prices.h5'],'/adj_prices')'; 
-       stk_status   = h5read([a.input_data_path,'\fdata\base_data\stk_status.h5'],'/stk_status')'; 
-       is_suspended = double(h5read([a.input_data_path,'\fdata\base_data\suspended.h5'],'/is_suspended')');
-       ipo_dates    = datenum_h5(h5read([a.input_data_path,'\fdata\base_data\securites_terms.h5'],'/ipo_date')); 
+        adj_prices   = h5read([a.input_data_path,'\fdata\base_data\stk_prices.h5'],'/adj_prices')'; 
+        stk_status   = h5read([a.input_data_path,'\fdata\base_data\stk_status.h5'],'/stk_status')'; 
+        is_suspended = double(h5read([a.input_data_path,'\fdata\base_data\suspended.h5'],'/is_suspended')');
+        ipo_dates    = datenum_h5(h5read([a.input_data_path,'\fdata\base_data\securites_terms.h5'],'/ipo_date')); 
 
-       is_suspended(isnan(stk_status)) = NaN;
-       is_suspended(is_suspended==1) = NaN;
-       is_suspended(isnan(is_suspended)) =1;
+        is_suspended(isnan(stk_status)) = NaN;
+        is_suspended(is_suspended==1) = NaN;
+        is_suspended(isnan(is_suspended)) =1;
 
-       for i = 1 : N
+        for i = 1 : N
            idx = find(p.all_trading_dates>=ipo_dates(i),1,'first');
            adj_prices(idx:idx + 21,i)  = NaN; % first 1 month set to NaN
-       end
+        end
 
-       adj_prices = adj_prices(1:T,:);  
-       adj_prices  = adj_table(adj_prices);
+        adj_prices = adj_prices(1:T,:);  
+        adj_prices  = adj_table(adj_prices);
 
-       for  i  = S  : T
-           for j = 1: N
-              if p.all_trading_dates(i)>ipo_dates(j)
-                 Y     = adj_prices(i-D2:i,j);
-                 sus   = is_suspended(i-D2+1:i,j);
-                 y     = Y(2:end)./Y(1:end-1)-1;  
-                % y(isnan(y))= 0;
-                 z = cumprod(1 + y);
-                 tao = sum(sus)/length(sus);%  停牌率
-                 if tao~=1
-                     if if_mix
-                         momentum_3m(i,j) = (1-tao*tao*tao)*(z(D2-D1)-1) + tao*tao*tao*vi(i,j); %#ok<UNRCH>
-                     else
-                         momentum_3m(i,j) = z(D2-D1)-1;
-                     end      
-                 else
-                     if if_mix
-                         momentum_3m(i,j) = vi(i,j); %#ok<UNRCH>
-                     else
-                         momentum_3m(i,j) = NaN;
-                     end      
-                 end
-              end
-           end
-       end
+        for  i  = S  : T
+            for j = 1: N
+                if p.all_trading_dates(i)>ipo_dates(j)
+                    Y     = adj_prices(i-D2:i,j);
+                    sus   = is_suspended(i-D2+1:i,j);
+                    y     = Y(2:end)./Y(1:end-1)-1;  
+                    % y(isnan(y))= 0;
+                    z = cumprod(1 + y);
+                    tao = sum(sus)/length(sus);%  停牌率
+                    if tao~=1
+                        momentum_3m(i,j) = z(D2-D1)-1;    
+                    else
+                        momentum_3m(i,j) = NaN;
+                    end
+                end
+            end
+        end
 
 
-       eval(['hdf5write(tgt_file, ''date'',p.all_trading_dates_, ''stk_code'',p.stk_codes_,' '''',...
+        eval(['hdf5write(tgt_file, ''date'',p.all_trading_dates_, ''stk_code'',p.stk_codes_,' '''',...
                 tgt_tag, ''',','' tgt_tag, ');']);  
     end
 
