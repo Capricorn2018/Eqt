70b975755e5cd545c2ba52850d4aff2b183cc151
Capricorn2018 <zhouxinyu882003@163.com>
2019-04-02 14:36:35 +0800

    daily_alpha_universe.m added

--- alpha_factors/main/daily_alpha_call.m
+++ alpha_factors/main/daily_alpha_call.m
@@ -8,10 +8,21 @@ cap_folder = 'D:/Projects/pit_data/mat/alpha_factors'; % tot_cap.h5所在folder
 
 trading_dates = h5read('D:/Capricorn/fdata/base_data/securites_dates.h5','/date');
 
+universe_folder = 'D:/Projects/pit_data/mat/alpha_factors/universe';
+
 % 从最新的pit data里面读一下s_info_windcode
 x = load('D:/Projects/pit_data/mat/income/pit_20190201.mat');
-stk_codes = x.s_info_windcode;
+stk_codes = x.data_last.s_info_windcode;
 stk_codes = unique(stk_codes);
+% 
+% for j = 1:length(trading_dates)
+%     
+%     universe = table(stk_codes);
+%     save([universe_folder,'/universe_',trading_dates{j},'.mat'],'universe');
+%     
+% end
 
-daily_alpha(stk_codes,trading_dates,input_folder,cap_folder,output_folder);
+factor_folder = input_folder;
+% daily_alpha(stk_codes,trading_dates,input_folder,cap_folder,output_folder);
+daily_alpha_universe(trading_dates,universe_folder,factor_folder,cap_folder,output_folder);
 
