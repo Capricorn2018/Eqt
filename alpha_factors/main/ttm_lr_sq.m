81fefd84bbd0ca229777c13a8aaa09b15edd580f
unknown <zhouxinyu882003@163.com>
2019-04-03 12:38:44 +0800

    some changes in ttm_lr_sq.m

--- alpha_factors/main/ttm_lr_sq.m
+++ alpha_factors/main/ttm_lr_sq.m
@@ -12,18 +12,18 @@
 % cap_type = 'float_cap';
 % market_cap( capital_folder, close_folder, stk_codes, output_folder, cap_type);
 % 
-% input_folder = 'D:/Projects/pit_data/mat/income';
-% db_names = {'net_profit_excl_min_int_inc','net_profit_incl_min_int_inc','oper_profit',...
-%                 'oper_rev','less_oper_cost','less_selling_dist_exp','less_gerl_admin_exp','less_fin_exp'};
-% output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
-% rpt_type = 'TTM';
-% calc_ttm_lr(input_folder, stk_codes, db_names, output_folder, rpt_type);
-% 
-% input_folder = 'D:/Projects/pit_data/mat/cashflow';
-% db_names = {'net_cash_flows_oper_act'};
-% output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
-% rpt_type = 'TTM';
-% calc_ttm_lr(input_folder, stk_codes, db_names, output_folder, rpt_type);
+input_folder = 'D:/Projects/pit_data/mat/income';
+db_names = {'net_profit_excl_min_int_inc','net_profit_incl_min_int_inc','oper_profit',...
+                'oper_rev','less_oper_cost','less_selling_dist_exp','less_gerl_admin_exp','less_fin_exp'};
+output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
+rpt_type = 'TTM';
+calc_ttm_lr(input_folder, db_names, output_folder, rpt_type);
+
+input_folder = 'D:/Projects/pit_data/mat/cashflow';
+db_names = {'net_cash_flows_oper_act'};
+output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
+rpt_type = 'TTM';
+calc_ttm_lr(input_folder, db_names, output_folder, rpt_type);
 
 input_folder = 'D:/Projects/pit_data/mat/balancesheet';
 db_names = {'tot_shrhldr_eqy_excl_min_int','tot_non_cur_liab','monetary_cap','tot_assets','tot_cur_assets','tot_cur_liab','tot_liab'};
@@ -32,36 +32,36 @@ rpt_type = 'LR';
 calc_ttm_lr(input_folder, db_names, output_folder, rpt_type);
 
 
-% input_folder = 'D:/Projects/pit_data/mat/income';
-% db_names = {'net_profit_excl_min_int_inc','net_profit_incl_min_int_inc','oper_profit',...
-%                 'oper_rev','less_oper_cost'};
-% output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
-% rpt_type = 'SQ';
-% calc_ttm_lr(input_folder, stk_codes, db_names, output_folder, rpt_type);
-% 
-% 
-% input_folder = 'D:/Projects/pit_data/mat/income';
-% db_names = {'net_profit_excl_min_int_inc'};
-% output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
-% rpt_type = 'LYR';
-% calc_ttm_lr(input_folder, stk_codes, db_names, output_folder, rpt_type);
-% 
-% input_folder = 'D:/Projects/pit_data/mat/income';
-% db_names = {'net_profit_excl_min_int_inc','oper_rev'};
-% output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
-% rpt_type = 'LTG';
-% calc_ttm_lr(input_folder, stk_codes, db_names, output_folder, rpt_type);
-% 
-% input_folder = 'D:/Projects/pit_data/mat/income';
-% db_names = {'net_profit_excl_min_int_inc','oper_rev','oper_profit'};
-% output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
-% rpt_type = 'YOY';
-% calc_ttm_lr(input_folder, stk_codes, db_names, output_folder, rpt_type);
-% 
-% input_folder = 'D:/Projects/pit_data/mat/balancesheet';
-% db_names = {'tot_assets'};
-% output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
-% rpt_type = 'MEAN';
-% calc_ttm_lr(input_folder, stk_codes, db_names, output_folder, rpt_type);
+input_folder = 'D:/Projects/pit_data/mat/income';
+db_names = {'net_profit_excl_min_int_inc','net_profit_incl_min_int_inc','oper_profit',...
+                'oper_rev','less_oper_cost'};
+output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
+rpt_type = 'SQ';
+calc_ttm_lr(input_folder, db_names, output_folder, rpt_type);
+
+
+input_folder = 'D:/Projects/pit_data/mat/income';
+db_names = {'net_profit_excl_min_int_inc'};
+output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
+rpt_type = 'LYR';
+calc_ttm_lr(input_folder, db_names, output_folder, rpt_type);
+
+input_folder = 'D:/Projects/pit_data/mat/income';
+db_names = {'net_profit_excl_min_int_inc','oper_rev'};
+output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
+rpt_type = 'LTG';
+calc_ttm_lr(input_folder, db_names, output_folder, rpt_type);
+
+input_folder = 'D:/Projects/pit_data/mat/income';
+db_names = {'net_profit_excl_min_int_inc','oper_rev','oper_profit'};
+output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
+rpt_type = 'YOY';
+calc_ttm_lr(input_folder, db_names, output_folder, rpt_type);
+
+input_folder = 'D:/Projects/pit_data/mat/balancesheet';
+db_names = {'tot_assets'};
+output_folder = 'D:/Projects/pit_data/mat/alpha_factors';
+rpt_type = 'MEAN';
+calc_ttm_lr(input_folder, db_names, output_folder, rpt_type);
 
 
