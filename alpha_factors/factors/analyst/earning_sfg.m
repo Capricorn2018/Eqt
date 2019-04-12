663a93f124b6abb7f56b0762fa6c9aaf20ae31a1
Capricorn2018 <zhouxinyu882003@163.com>
2019-04-12 16:41:28 +0800

    factor_append.m bugs fixed

--- alpha_factors/factors/analyst/earning_sfg.m
+++ alpha_factors/factors/analyst/earning_sfg.m
@@ -1,23 +1,81 @@
 function [] = earning_sfg(a,p)
 % earning_sfg 未来12个月预期净利润增长率
 
-    T = length(p.all_trading_dates );
-    N = length(p.stk_codes);   
-    tgt_file =  [a.output_data_path,'/earning_sfg.h5'];
-    tgt_tag = 'earning_sfg'; 
-    [S,earning_sfg] =  check_exist(tgt_file,'/earning_sfg',p,T,N);
+%     T = length(p.all_trading_dates );
+%     N = length(p.stk_codes);   
+%     tgt_file =  [a.output_data_path,'/earning_sfg.h5'];
+%     tgt_tag = 'earning_sfg'; 
+%     [S,earning_sfg] =  check_exist(tgt_file,'/earning_sfg',p,T,N);
+%     
+%     if S>0
+%         
+%         stk_code = p.stk_codes;        
+%         
+%         for i=S:T
+%             
+%             sdt = p.all_trading_dates_{i};
+%             %sdt = int2str(dt);
+%             sdt = [sdt(1:4),'-',sdt(5:6),'-',sdt(7:8)];
+%             
+%             file = ['D:\Capricorn\DB\zyyx\daily\con_forecast_roll_stk\con_forecast_roll_stk_',sdt,'.mat'];
+%             
+%             if exist(file,'file')==2
+%                 load(file);
+%             else
+%                 continue;
+%             end
+%             
+%             eps_stk = cell_s2l(t.STOCK_CODE); % 把朝阳永续里的股票代码补齐
+%             
+%             % 一致预期净利润增长率
+%             eps = t.CON_NP_YOY_ROLL; % 要替换的是这里
+%             
+%             [all_stk,~,~] = union(eps_stk,stk_code);
+%             [~,eps_i] = ismember(eps_stk,all_stk);
+%             [~,stk_i] = ismember(stk_code,all_stk);
+%             
+%             if length(all_stk) > length(stk_code)
+%                 tmp = nan(size(earning_sfg,1),length(all_stk));
+%                 tmp(:,stk_i) = earning_sfg;
+%                 earning_sfg = tmp;
+%                 stk_code = all_stk;                
+%             end
+%             
+%             earning_sfg(i,eps_i) = eps;
+%                     
+%         end        
+%         
+%         if exist(tgt_file,'file')==2
+%             eval(['delete ',tgt_file]);
+%         end
+%         
+%         eval(['hdf5write(tgt_file, ''date'',p.all_trading_dates_, ''stk_code'',stk_code,' '''',tgt_tag, ''',','' tgt_tag, ');']);  
+%     end
+
+    tgt_file = [a.output_data_path,'/earning_sfg.mat'];
+    if exist(tgt_file,'file')==2
+        earning_sfg = load(tgt_file);
+        dt = earning_sfg.data.DATEN;
+        dt_max = max(dt);
+        bool = true;
+    else
+        dt_max = 0;
+        bool = false;
+    end    
     
-    if S>0
+    
+    if dt_max<=p.all_trading_dates(end)
         
-        stk_code = p.stk_codes;        
+        S = find(p.all_trading_dates>dt_max,1);
         
-        for i=S:T
-            
+        for i = S:length(p.all_trading_dates)
+        
             sdt = p.all_trading_dates_{i};
-            %sdt = int2str(dt);
             sdt = [sdt(1:4),'-',sdt(5:6),'-',sdt(7:8)];
             
-            file = ['D:\Capricorn\DB\zyyx\daily\con_forecast_roll_stk\con_forecast_roll_stk_',sdt,'.mat'];
+            DATEN = datenum(p.all_trading_dates_{i},'yyyymmdd');
+            
+            file = [a.zyyx_path,'/con_forecast_roll_stk/con_forecast_roll_stk_',sdt,'.mat'];
             
             if exist(file,'file')==2
                 load(file);
@@ -25,31 +83,34 @@ function [] = earning_sfg(a,p)
                 continue;
             end
             
-            eps_stk = cell_s2l(t.STOCK_CODE); % 把朝阳永续里的股票代码补齐
+            append = struct();
+            append.data = table();
+            append.code_map = table();
             
-            % 一致预期净利润增长率
-            eps = t.CON_NP_YOY_ROLL; % 要替换的是这里
+            append.data.stk_codes = cell_s2l(t.STOCK_CODE); % 把朝阳永续里的股票代码补齐
+            append.data.DATEN = ones(height(t),1) * DATEN;
+            append.data.earning_sfg = t.CON_NP_YOY_ROLL;
             
-            [all_stk,~,~] = union(eps_stk,stk_code);
-            [~,eps_i] = ismember(eps_stk,all_stk);
-            [~,stk_i] = ismember(stk_code,all_stk);
+            append.code_map.stk_codes = unique(append.data.stk_codes);
+            append.code_map.stk_num = (1:height(append.code_map))';
             
-            if length(all_stk) > length(stk_code)
-                tmp = nan(size(earning_sfg,1),length(all_stk));
-                tmp(:,stk_i) = earning_sfg;
-                earning_sfg = tmp;
-                stk_code = all_stk;                
+            [~,Locb] = ismember(append.data.stk_codes,append.code_map.stk_codes);
+            append.data.stk_num = append.code_map.stk_num(Locb);
+            
+            append.data = append.data(:,{'DATEN','stk_num','earning_sfg'});
+            
+            if exist('earning_sfg','var')==1
+                earning_sfg = factor_append(earning_sfg,append);
+            else
+                earning_sfg = append;
             end
-            
-            earning_sfg(i,eps_i) = eps;
-                    
-        end        
-        
-        if exist(tgt_file,'file')==2
-            eval(['delete ',tgt_file]);
+                        
         end
         
-        eval(['hdf5write(tgt_file, ''date'',p.all_trading_dates_, ''stk_code'',stk_code,' '''',tgt_tag, ''',','' tgt_tag, ');']);  
+        data = earning_sfg.data; %#ok<NASGU>
+        code_map = earning_sfg.code_map; %#ok<NASGU>
+        eval(['save(''',tgt_file,''',''data'',''code_map'');']);        
+        
     end
     
 
--- alpha_factors/utility/factor_append.m
+++ alpha_factors/utility/factor_append.m
@@ -5,7 +5,9 @@ function z = factor_append(x , append)
     code_map1 = x.code_map;
     code_map2 = append.code_map;
 
-    C = outerjoin(code_map1,code_map2,'Keys',{'stk_codes'});
+    C = outerjoin(code_map1,code_map2,'Keys',{'stk_codes'},'Type','full');
+    C.stk_codes = C.stk_codes_code_map1;
+    C.stk_codes(isnan(C.stk_num_code_map1)) = C.stk_codes_code_map2(isnan(C.stk_num_code_map1));
     C = sortrows(C,{'stk_codes'},{'ascend'});
     
     stk_num1 = C.stk_num_code_map1;
@@ -14,11 +16,11 @@ function z = factor_append(x , append)
     stk_num1(isnan(stk_num1)) = ((max([stk_num1;0])+1):(max([stk_num1;0])+count))';
     C.stk_num_code_map1 = stk_num1;
     
-    append_data = join(append.data,C,'LeftKeys',{'stk_num'},'RightKeys',{'stk_num_code_map2'},'RightVariables',{'stk_num_code_map1'},'Typ2','left');
+    append_data = outerjoin(append.data,C,'LeftKeys',{'stk_num'},'RightKeys',{'stk_num_code_map2'},'RightVariables',{'stk_num_code_map1'},'Type','left');
     append_data.stk_num = append_data.stk_num_code_map1;
     append_data = append_data(:,x.data.Properties.VariableNames);
     
-    data = [x.data,append_data];
+    data = [x.data;append_data];
     C.stk_num = C.stk_num_code_map1;
     code_map = C(:,{'stk_codes','stk_num'});
     
