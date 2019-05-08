2bddd1a6fcdbf0944f3d0e36fc5ab5c3d909251a
Capricorn2018 <zhouxinyu882003@163.com>
2019-05-08 13:58:22 +0800

    call.m revised

--- model/portfolio_construction/main/call.m
+++ model/portfolio_construction/main/call.m
@@ -1,13 +1,13 @@
 % 需要做优化的日期序列
-p.rebalance_dates % 这个也应该是从setting文件里面导入
+rebalance_dates % 这个也应该是从setting文件里面导入
 
 % 先确定universe
 % 从excel或是什么数据源读取每日universe
-p.universe % 这个存在一个list里面
+universe % 这个存在一个list里面, 要对应到每个rebalance_dates
 
 % 然后确定需要的alpha因子文件
-p.alpha_names = {'ep_lyr'}; % 这里就用文件名，最好是以后从setting文件里面导入
-
+% alpha因子的名称, 可以在存参数的表里面再弄一个因子名称和文件地址的映射表
+factor_names = {'ep_lyr'}; 
 
 % 按日循环算weights
 portfolio_construction
